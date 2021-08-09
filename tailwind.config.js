72316085236237b1b935d0b89862821fc074c754
Krushil Naik <krushilnaik96@gmail.com>
2021-08-09 18:05:53 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/pages/**/*.{ts,tsx,js,jsx}'],
+	theme: {
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+};

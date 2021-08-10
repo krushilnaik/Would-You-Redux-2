ecde30ba9195d34cbfa082e398818257e16c73f9
Krushil Naik <krushilnaik96@gmail.com>
2021-08-10 09:05:25 -0400

    Add nav bar to header
    - NOTE: might swap trapezoid PNG with an SVG later

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,28 @@
 import React, { ReactElement } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface Props {}
 
 function Header({}: Props): ReactElement {
-	return <header className='border-b-4 border-purple-700'>HEADER</header>;
+	const router = useRouter();
+	const url = router.pathname;
+
+	return (
+		<header className='flex border-b-2 border-purple-700'>
+			<ul className='self-end flex gap-3'>
+				<li className={`link ${url === '/' ? 'active' : ''}`}>
+					<Link href='/'>Home</Link>
+				</li>
+				<li className={`link ${url === '/add' ? 'active' : ''}`}>
+					<Link href='/add'>New Question</Link>
+				</li>
+				<li className={`link ${url === '/leaderboard' ? 'active' : ''}`}>
+					<Link href='/leaderboard'>Leader Board</Link>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
